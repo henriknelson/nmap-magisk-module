2797a290cbff3856c17f32ae5edd0a4a7ef4ed7b
Henrik Nelson <henrik@cliffords.nu>
2019-08-27 14:32:00 +0200

    Added module files

---
+++ README.md
@@ -0,0 +1,19 @@
+# NMap for Android (aarch64)
+
+## by nelshh @ xda-developers
+
+This Magisk module installs the latest stable version of NMap to the target system
+
+## Change Log
+
+### v1.0 - 2019-08-07
+* Initial release
+
+## Source Code
+* Source [svn.nmap.org](https://svn.nmap.org/nmap/)
+
+## Module Source Code
+* Module [GitHub](https://github.com/henriknelson/nmap-magisk-module)
+
+## Contact
+* Developer: [henrik@cliffords.nu](mailto:henrik@cliffords.nu)
---
+++ common/service.sh
@@ -0,0 +1,61 @@
+#!/system/bin/sh
+# Do NOT assume where your module will be located.
+# ALWAYS use $MODDIR if you need to know where this script
+# and module is placed.
+# This will make sure your module will still work
+# if Magisk change its mount point in the future
+MODDIR=${0%/*};
+
+symlink_from_file() {
+   cat $1 | while read line
+   do
+      target=$(echo $line | sed 's/;/ /g' | awk '{printf $2}');
+      symlink=$(echo $line | sed 's/;/ /g' | awk '{printf $1}');
+      ln -sf $target $2/$symlink;
+      chown 0:0 $2/$symlink;
+      chmod 755 $2/$symlink;
+   done
+   chown -R 0:0 $2;
+   chmod -R 755 $2;
+}
+
+cp_man() {
+   man_dir=$1;
+   echo $man_dir;
+   if [[ ! -d "/system/usr/share/man/$man_dir" ]]; then
+      mkdir -p /system/usr/share/man/$man_dir;
+   fi
+
+   for man_file in $MODDIR/system/usr/share/man/$man_dir/*; do
+      cp -f $MODDIR/system/usr/share/man/$man_dir/$man_file /system/usr/share/man/$man_dir;
+      chown 0:0 /system/usr/share/man/$man_dir/$man_file;
+      chmod 644 /system/usr/share/man/$man_dir/$man_file;
+   done
+}
+
+mount -o rw,remount /system;
+
+cd /system/bin;
+cp $MODDIR/symlinks/symlinks_bin .;
+symlink_from_file "$(pwd)/symlinks_bin" "$(pwd)";
+
+mount -o ro,remount /system;
+
+mount -o rw,remount /system/usr/share/man;
+
+cd /system/usr/share/man/man1;
+cp $MODDIR/symlinks/symlinks_man .;
+symlink_from_file "$(pwd)/symlinks_man" "$(pwd)";
+
+
+for dir in $MODDIR/system/usr/share/man/man*/; do
+    dir="${dir%/}";
+    dir="${dir##*/}";
+    cp_man $dir;
+done
+
+if [[ -s "/system/bin/mandoc" ]]; then
+  makewhatis /system/usr/share/man;
+fi
+
+mount -o ro,remount /system/usr/share/man;
